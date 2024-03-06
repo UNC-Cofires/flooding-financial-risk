86beca54b074995a7848d3d6c1d5bd570db3e134
Kieran Patrick Fitzmaurice <kpf10@pitt.edu>
2024-03-06 12:42:38 -0500

    modified kriging to allow for splitting across multiple runs

--- multiple_events/analysis/property_value_estimation.py
+++ multiple_events/analysis/property_value_estimation.py
@@ -6,6 +6,8 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import datetime as dt
+import pyarrow as pa
+import pyarrow.parquet as pq
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold,GridSearchCV
 import spacetimekriging as stk
@@ -94,12 +96,22 @@ quarters = dates.to_period('Q')
 q_start = dates.is_quarter_start
 q_end = dates.is_quarter_end
 dates = pd.to_datetime((dates[q_start] + (dates[q_end] - dates[q_start])/2).date)
+periods = dates.to_period('Q').astype(str)
+
+# Check to see if we already performed kriging for this period in a previous run
+previous_runs = np.sort([x.split('_')[-1].split('.parquet')[0] for x in os.listdir(outfolder) if 'kriged' in x])
+
+# Drop periods that we ran previously
+m = (~periods.isin(previous_runs))
+dates = dates[m]
+periods = periods[m]
 
 timepoint_list = []
 
-for date in dates:
+for date,period in zip(dates,periods):
     temp = homes.copy()
     temp['date'] = date
+    temp['period'] = period
     timepoint_list.append(temp)
     
 property_timepoints = pd.concat(timepoint_list).reset_index(drop=True)
@@ -234,66 +246,70 @@ kf = KFold(n_splits=k,random_state=None,shuffle=True)
 
 cv_list = []
 
-for i,(train_indices,test_indices) in enumerate(kf.split(sales)):
-    
-    # Get hard points and kriging interpolation points 
-    hard_points = sales.iloc[train_indices].copy()
-    krig_points = sales.iloc[test_indices].copy()
-    krig_points = krig_points[krig_points['study_area']==1]
-    
-    X_h = hard_points[features].to_numpy()
-    X_k = krig_points[features].to_numpy()
-    y_h = hard_points[response_variable].to_numpy()
-    
-    # Determine optimal hyperparameters for mean trend random forest model
-    param_grid = {'n_estimators':[200],'max_depth':[7,9,11],'max_features':[0.33,0.5,0.66]}
-    param_grid = {}
-    grid_search = GridSearchCV(RandomForestRegressor(), param_grid=param_grid, cv=k-1, scoring='neg_mean_squared_error',n_jobs=n_cores)
-    grid_search.fit(X_h,y_h)
-    hyperparams = grid_search.best_params_
-    
-    # Fit model to hard point data
-    model = RandomForestRegressor(**hyperparams).fit(X_h,y_h)
-    
-    # Get residuals at hard points
-    z_h = y_h - model.predict(X_h)
-    hard_points['residual'] = z_h
-    
-    # Estimate residuals at krig points via simple kriging (assume zero mean)
-    Cs = stk.SphCovFun(a_bounds=(250,5000))
-    Ct = stk.ExpCovFun(a=365)
-    Cst = stk.ProductSumSTCovFun(Cs,Ct,k3=0,nugget_bounds=(0,2),k1_bounds=(0,2),k2_bounds=(0,2))
-    SK = stk.SimpleKriging(hard_points,krig_points,'residual',spatial_column='geometry',temporal_column='time_val')    
-    SK.build_distance_matrices(5000,np.inf)
-    SK.estimate_variogram(Cst,spatial_lags,temporal_lags,options={'tol':0.0001})
-    z_k,sigma_k = SK.krig_values(n_max=100,n_min=10)
-    
-    y_k = z_k + model.predict(X_k)
-    sigma_k[np.isnan(sigma_k)] = np.std(SK.z_h)
-    
-    krig_points['log_val_transfer_kriged'] = y_k
-    krig_points['sigma_log_val_transfer_kriged'] = sigma_k
-    krig_points['val_transfer_kriged'] = np.exp(y_k + 0.5*sigma_k**2)
-    krig_points['sigma_val_transfer_kriged'] = krig_points['val_transfer_kriged']**2*(np.exp(sigma_k**2)-1)
-    krig_points['log_val_transfer_RF'] = model.predict(X_k) 
-    cv_list.append(krig_points)
-    
-cv_df = pd.concat(cv_list).reset_index(drop=True)
+# First check to see if cross-validation was already performed as part of a previous run
+cv_outname = os.path.join(outfolder,f'group_{group_idx}_cross_validation.parquet')
 
-# Calculate error metrics
-cv_df['abs_error_log'] = np.abs((cv_df['log_val_transfer'] - cv_df['log_val_transfer_kriged']))
-cv_df['abs_percent_error_log'] = cv_df['abs_error_log']/np.abs((cv_df['log_val_transfer']))
-cv_df['abs_error_nonlog'] = np.abs((cv_df['val_transfer'] - cv_df['val_transfer_kriged']))
-cv_df['abs_percent_error_nonlog'] = cv_df['abs_error_nonlog']/np.abs((cv_df['val_transfer']))
+if not os.path.exists(cv_outname):
 
-# Save cross-validation results
-cv_outname = os.path.join(pv_folder,'property_value_cross_validation.gdb')
-cv_df.to_file(cv_outname,layer=f'group_{group_idx}',driver='OpenFileGDB')
+    for i,(train_indices,test_indices) in enumerate(kf.split(sales)):
+        
+        print(f'\n*** CV fold {i+1} / {k} ***\n',flush=True)
 
-cv_object_outname = os.path.join(outfolder,f'group_{group_idx}_cross_validation.object')
-with open(cv_object_outname,'wb') as f:
-    pickle.dump(cv_df,f)
-    f.close()
+        # Get hard points and kriging interpolation points 
+        hard_points = sales.iloc[train_indices].copy()
+        krig_points = sales.iloc[test_indices].copy()
+        krig_points = krig_points[krig_points['study_area']==1]
+
+        X_h = hard_points[features].to_numpy()
+        X_k = krig_points[features].to_numpy()
+        y_h = hard_points[response_variable].to_numpy()
+
+        # Determine optimal hyperparameters for mean trend random forest model
+        param_grid = {'n_estimators':[200],'max_depth':[7,9,11],'max_features':[0.33,0.5,0.66]}
+        grid_search = GridSearchCV(RandomForestRegressor(), param_grid=param_grid, cv=k-1, scoring='neg_mean_squared_error',n_jobs=n_cores)
+        grid_search.fit(X_h,y_h)
+        hyperparams = grid_search.best_params_
+
+        # Fit model to hard point data
+        model = RandomForestRegressor(**hyperparams).fit(X_h,y_h)
+
+        # Get residuals at hard points
+        z_h = y_h - model.predict(X_h)
+        hard_points['residual'] = z_h
+
+        # Estimate residuals at krig points via simple kriging (assume zero mean)
+        Cs = stk.SphCovFun(a_bounds=(250,5000))
+        Ct = stk.ExpCovFun(a=365)
+        Cst = stk.ProductSumSTCovFun(Cs,Ct,k3=0,nugget_bounds=(0,2),k1_bounds=(0,2),k2_bounds=(0,2))
+        SK = stk.SimpleKriging(hard_points,krig_points,'residual',spatial_column='geometry',temporal_column='time_val')    
+        SK.build_distance_matrices(5000,np.inf)
+        SK.estimate_variogram(Cst,spatial_lags,temporal_lags,options={'tol':0.0001})
+        z_k,sigma_k = SK.krig_values(n_max=100,n_min=10)
+
+        y_k = z_k + model.predict(X_k)
+        sigma_k[np.isnan(sigma_k)] = np.std(SK.z_h)
+
+        krig_points['log_val_transfer_kriged'] = y_k
+        krig_points['sigma_log_val_transfer_kriged'] = sigma_k
+        krig_points['val_transfer_kriged'] = np.exp(y_k + 0.5*sigma_k**2)
+        krig_points['sigma_val_transfer_kriged'] = krig_points['val_transfer_kriged']**2*(np.exp(sigma_k**2)-1)
+        krig_points['log_val_transfer_RF'] = model.predict(X_k) 
+        cv_list.append(krig_points)
+
+    cv_df = pd.concat(cv_list).reset_index(drop=True)
+
+    # Calculate error metrics
+    cv_df['abs_error_log'] = np.abs((cv_df['log_val_transfer'] - cv_df['log_val_transfer_kriged']))
+    cv_df['abs_percent_error_log'] = cv_df['abs_error_log']/np.abs((cv_df['log_val_transfer']))
+    cv_df['abs_error_nonlog'] = np.abs((cv_df['val_transfer'] - cv_df['val_transfer_kriged']))
+    cv_df['abs_percent_error_nonlog'] = cv_df['abs_error_nonlog']/np.abs((cv_df['val_transfer']))
+
+    # Save cross-validation results
+    cv_df = cv_df.drop(columns='geometry')
+    cv_df.to_parquet(cv_outname)
+    
+else:
+    print('Cross-validation already performed in a previous run.',flush=True)
     
 ### *** RANDOM FOREST REGRESSION KRIGING PREDICTION *** ###
 
@@ -314,52 +330,60 @@ model = RandomForestRegressor(**hyperparams).fit(X_h,y_h)
 z_h = y_h - model.predict(X_h)
 sales['residual'] = z_h
 
-# To reduce memory consumption, perform kriging in chunks
-chunksize=150000 - len(sales)
-n_chunks = np.ceil(len(property_timepoints)/chunksize).astype(int)
-krig_list = []
-
-for i,krig_points in enumerate(np.array_split(property_timepoints,n_chunks)):
+# Save progress for each period in case it takes multiple runs to complete
+for period in periods:
     
-    print(f'\n*** Chunk {i+1} / {n_chunks} ***\n',flush=True)
+    period_points = property_timepoints[property_timepoints['period']==period]
+
+    # To reduce memory consumption, perform kriging in chunks
+    chunksize = 250000 - len(sales)
+    n_chunks = np.ceil(len(period_points)/chunksize).astype(int)
     
-    X_k = krig_points[features].to_numpy()
+    krig_list = []
 
-    # Estimate residuals at krig points via simple kriging (assume zero mean)
-    Cs = stk.SphCovFun(a_bounds=(250,5000))
-    Ct = stk.ExpCovFun(a=365)
-    Cst = stk.ProductSumSTCovFun(Cs,Ct,k3=0,nugget_bounds=(0,2),k1_bounds=(0,2),k2_bounds=(0,2))
-    SK = stk.SimpleKriging(sales,krig_points,'residual',spatial_column='geometry',temporal_column='time_val')    
-    SK.build_distance_matrices(5000,np.inf)
-    SK.estimate_variogram(Cst,spatial_lags,temporal_lags,options={'tol':0.0001})
-    z_k,sigma_k = SK.krig_values(n_max=100,n_min=10)
+    for i,krig_points in enumerate(np.array_split(period_points,n_chunks)):
+        
+        print(f'\n*** {period}: Chunk {i+1} / {n_chunks} ***\n',flush=True)
+        
+        X_k = krig_points[features].to_numpy()
 
-    y_k = z_k + model.predict(X_k)
-    sigma_k[np.isnan(sigma_k)] = np.std(SK.z_h)
+        # Estimate residuals at krig points via simple kriging (assume zero mean)
+        Cs = stk.SphCovFun(a_bounds=(250,5000))
+        Ct = stk.ExpCovFun(a=365)
+        Cst = stk.ProductSumSTCovFun(Cs,Ct,k3=0,nugget_bounds=(0,2),k1_bounds=(0,2),k2_bounds=(0,2))
+        SK = stk.SimpleKriging(sales,krig_points,'residual',spatial_column='geometry',temporal_column='time_val')    
+        SK.build_distance_matrices(5000,np.inf)
+        SK.estimate_variogram(Cst,spatial_lags,temporal_lags,options={'tol':0.0001})
+        z_k,sigma_k = SK.krig_values(n_max=100,n_min=10)
 
-    krig_points['log_val_transfer_kriged'] = y_k
-    krig_points['sigma_log_val_transfer_kriged'] = sigma_k
-    krig_points['val_transfer_kriged'] = np.exp(y_k + 0.5*sigma_k**2)
-    krig_points['sigma_val_transfer_kriged'] = krig_points['val_transfer_kriged']**2*(np.exp(sigma_k**2)-1)
-    krig_points['log_val_transfer_RF'] = model.predict(X_k)
-    
-    krig_list.append(krig_points)
-    gc.collect()
-    
-kriged_df = pd.concat(krig_list)
-kriged_df = kriged_df.drop(columns='geometry')
-kriged_df = kriged_df.sort_values(by=['building_id','date']).reset_index(drop=True)
+        y_k = z_k + model.predict(X_k)
+        sigma_k[np.isnan(sigma_k)] = np.std(SK.z_h)
 
-### *** SAVE RESULTS *** ###
-kriged_outname = os.path.join(outfolder,f'group_{group_idx}_property_values_kriged.csv')
-kriged_df.to_csv(kriged_outname,index=False)
+        krig_points['log_val_transfer_kriged'] = y_k
+        krig_points['sigma_log_val_transfer_kriged'] = sigma_k
+        krig_points['val_transfer_kriged'] = np.exp(y_k + 0.5*sigma_k**2)
+        krig_points['sigma_val_transfer_kriged'] = krig_points['val_transfer_kriged']**2*(np.exp(sigma_k**2)-1)
+        krig_points['log_val_transfer_RF'] = model.predict(X_k)
 
-covfun_outname = os.path.join(outfolder,f'group_{group_idx}_covariance_function.object')
-with open(covfun_outname,'wb') as f:
-    pickle.dump(SK.Cst,f)
-    f.close()
-    
-variogram_outname = os.path.join(outfolder,f'group_{group_idx}_empirical_variogram.object')
-with open(variogram_outname,'wb') as f:
-    pickle.dump(SK.empirical_variogram,f)
-    f.close()
+        krig_list.append(krig_points)
+        gc.collect()
+        
+    kriged_df = pd.concat(krig_list)
+    kriged_df = kriged_df.drop(columns='geometry')
+    kriged_df = kriged_df.sort_values(by=['building_id','date']).reset_index(drop=True)
+
+    ### *** SAVE RESULTS *** ###
+    kriged_outname = os.path.join(outfolder,f'group_{group_idx}_property_values_kriged_{period}.parquet')
+    kriged_df.to_parquet(kriged_outname)
+
+    covfun_outname = os.path.join(outfolder,f'group_{group_idx}_covariance_function.object')
+    if not os.path.exists(covfun_outname):
+        with open(covfun_outname,'wb') as f:
+            pickle.dump(SK.Cst,f)
+            f.close()
+
+    variogram_outname = os.path.join(outfolder,f'group_{group_idx}_empirical_variogram.object')
+    if not os.path.exists(variogram_outname):
+        with open(variogram_outname,'wb') as f:
+            pickle.dump(SK.empirical_variogram,f)
+            f.close()
--- multiple_events/analysis/property_value_estimation.sh
+++ multiple_events/analysis/property_value_estimation.sh
@@ -3,11 +3,12 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=350g
-#SBATCH -t 5-00:00:00
+#SBATCH --mem=250g
+#SBATCH -t 11-00:00:00
 #SBATCH --mail-type=all
+#SBATCH --job-name=pv_estimation
 #SBATCH --mail-user=kieranf@email.unc.edu
-#SBATCH --array=0-1
+#SBATCH --array=31-40
 cd /proj/characklab/flooddata/NC/multiple_events/analysis
 module purge
 module load anaconda/2023.03
