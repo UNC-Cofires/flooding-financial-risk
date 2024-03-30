6f88046b352dde702a21198b51d088ace862ad9c
Kieran Patrick Fitzmaurice <kpf10@pitt.edu>
2024-03-29 22:32:39 -0400

    suppressed numpy warnings

--- multiple_events/analysis/borrower_simulation.sh
+++ multiple_events/analysis/borrower_simulation.sh
@@ -8,9 +8,10 @@
 #SBATCH --mail-type=all
 #SBATCH --job-name=borrower_sim
 #SBATCH --mail-user=kieranf@email.unc.edu
-#SBATCH --array=24
+#SBATCH --array=63,77,66,23,70,68,32,8,73,97,30,9,41,64,51,95,15,53,7,81,6,93,27,25,47,39,45,46,36,88,82
 cd /proj/characklab/flooddata/NC/multiple_events/analysis
 module purge
 module load anaconda/2023.03
+export PYTHONWARNINGS="ignore"
 conda activate /proj/characklab/flooddata/NC/conda_environments/envs/flood_v1
 python3.11 borrower_simulation.py
